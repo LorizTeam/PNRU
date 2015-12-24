9e144ccc2384dd5b86793c5438b82def635364bc
kongsattra <kongsattra@gmail.com>
2015-12-24 16:08:12 +0700

    qwer

--- includehtml1.js
+++ includehtml1.js
@@ -1,10 +1,3 @@
 window.onload=function(){
-		var ctx = document.getElementById("canvas").getContext("2d");
-				window.myBar = new Chart(ctx).Bar(barChartData, {
-					responsive : true
-				});
-				var ctx = document.getElementById("canvasbar").getContext("2d");
-				window.myBar = new Chart(ctx).Bar(barChartData, {
-					responsive : true
-				});
+	
 	function a(a,b){var c=/^(?:file):/,d=new XMLHttpRequest,e=0;d.onreadystatechange=function(){4==d.readyState&&(e=d.status),c.test(location.href)&&d.responseText&&(e=200),4==d.readyState&&200==e&&(a.outerHTML=d.responseText)};try{d.open("GET",b,!0),d.send()}catch(f){}}var b,c=document.getElementsByTagName("*");for(b in c)c[b].hasAttribute&&c[b].hasAttribute("data-include")&&a(c[b],c[b].getAttribute("data-include"))};
